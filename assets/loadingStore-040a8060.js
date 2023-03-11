c76224cf996545abef210e3032fe2be2e447e9f6
wayne <n0918679182@gmail.com>
2023-03-11 16:37:22 +0800

    deploy

---
+++ assets/loadingStore-040a8060.js
@@ -0,0 +1,1 @@
+import{d as i}from"./index-65b05873.js";const t=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},500)}}});export{t as l};
--- assets/loadingStore-098cc55d.js
+++
@@ -1,1 +0,0 @@
-import{d as i}from"./index-7f7f8b79.js";const t=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},500)}}});export{t as l};
