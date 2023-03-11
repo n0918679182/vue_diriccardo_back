0cdfd8342a943eeee9b83665cb949c3b754cb9cd
wayne <n0918679182@gmail.com>
2023-03-11 16:09:27 +0800

    deploy

--- assets/loadingStore-4de37305.js
+++
@@ -1,1 +0,0 @@
-import{d as i}from"./index-039d7c0c.js";const t=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},500)}}});export{t as l};
